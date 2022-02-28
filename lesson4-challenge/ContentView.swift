05e16d190243690991d8f78b2614809a51569791
MeenakshiPoorani <smeenakshipoorani@gmail.com>
2022-03-01 01:53:16 +0530

    Use only 1 GeometryReader

--- lesson4-challenge/ContentView.swift
+++ lesson4-challenge/ContentView.swift
@@ -11,28 +11,25 @@ struct ContentView: View {
     var body: some View {
         VStack(spacing: 0.0)
         {
+            
+            //using just 1 geometry reader
+            //NOTE : Use geo.size inside .frame modifier to set size & use geo.frame(in : .local).minX etc. inside .position modifier
+            
             GeometryReader{ geo in
-                Rectangle().foregroundColor(.green)
-                Rectangle().foregroundColor(.red).frame(width: 2/3*geo.size.width, height: geo.size.height/4).position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).midY)
-            }
-            GeometryReader{ geo in
-                /*
-                HStack(spacing: 0.0) {
-                    Rectangle().foregroundColor(.purple).frame(width: geo.size.width/2, height: geo.size.height)
-                    Rectangle().foregroundColor(.orange).frame(width: geo.size.width/2, height: geo.size.height)
-                }*/
-                
-                Rectangle().foregroundColor(.purple).frame(width: geo.size.width/2, height: geo.size.height)
-                Rectangle().foregroundColor(.orange).frame(width: geo.size.width/2, height: geo.size.height).position(x: geo.frame(in: .local).maxX*3/4, y: geo.frame(in: .local).midY)
+                Rectangle().foregroundColor(.green).frame(width: geo.size.width, height: geo.size.height/2)
+                Rectangle().foregroundColor(.red).frame(width: 2/3*geo.size.width, height: geo.size.height/8).position(x: geo.frame(in: .local).maxX/2, y: geo.frame(in: .local).maxY/4)
+            
+                Rectangle().foregroundColor(.purple).frame(width: geo.size.width/2, height: geo.size.height/2).position(x: geo.frame(in: .local).maxX/4, y: geo.frame(in: .local).maxY*3/4)
+                Rectangle().foregroundColor(.orange).frame(width: geo.size.width/2, height: geo.size.height/2).position(x: geo.frame(in: .local).maxX*3/4, y: geo.frame(in: .local).maxY*3/4)
                
-            }
+            
         }.ignoresSafeArea()
     }
 }
-
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .previewDevice("iPhone 11")
+            .previewDevice("iPhone 13")
     }
 }
